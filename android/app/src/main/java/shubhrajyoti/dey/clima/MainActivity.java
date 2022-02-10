ec40b9c33f7aac01785742798af2decb418bea5a
shubhrajyotidey305 <shubhrajyotidey305@gmail.com>
2022-02-10 21:28:49 +0530

    first commit of this app

---
+++ android/app/src/main/java/shubhrajyoti/dey/clima/MainActivity.java
@@ -0,0 +1,6 @@
+package shubhrajyoti.dey.clima;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,42 @@
+import 'package:clima/services/weather.dart';
+import 'package:flutter/material.dart';
+import 'package:clima/screens/location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocationData() async {
+    var weatherData = await WeatherModel().getLocation();
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => LocationScreen(
+                locationData: weatherData,
+              )),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
+  }
+}
